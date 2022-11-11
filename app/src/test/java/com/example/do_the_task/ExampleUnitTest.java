6b45a6be620b075c1a9bbd2f5a8e13e65f245900
s2171045 <s2171045@s.konan-u.ac.jp>
2022-11-11 17:24:57 +0900

    入力欄ちょっと進めました

--- app/src/test/java/com/example/do_the_task/ExampleUnitTest.java
+++ app/src/test/java/com/example/do_the_task/ExampleUnitTest.java
@@ -4,17 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        System.out.println("福原勇樹");
-        System.out.println("岡本英也");
-        System.out.println("近藤凛太郎");
-        //assertEquals(4, 2 + 2);
+
     }
 }
