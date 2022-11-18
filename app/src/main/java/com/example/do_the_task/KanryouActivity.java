c08caced6358472c6b32c291f247c914d7b708b4
s2071106 <yuuki.fukuhara@icloud.com>
2022-11-18 16:41:46 +0900

    完了画面と未完了画面作りました

---
+++ app/src/main/java/com/example/do_the_task/KanryouActivity.java
@@ -0,0 +1,25 @@
+package com.example.do_the_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class KanryouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kanryou);
+        //表示画面に戻る
+        Button nextButton = (Button) findViewById(R.id.button3);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(KanryouActivity.this, HyouziActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
